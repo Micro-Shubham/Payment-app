cf0198f84efb5bfd72184c7a8b97dd40f1ef7b45
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-22 19:14:07 +0530

    add zod validation for user signup and implement user creation logic

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,7 +1,48 @@
 const express = require("express")
+const {User} = require("../db")
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = require("../config")
+const zod = require("zod")
 
 const router = express.Router()
 
+const signupSchema = zod.object ({
+    username: zod.string().email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
 
+
+// sign up routes
+router.post("/signup", async (req,res) =>{ 
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body);
+   if(!success) {
+    return res.json({
+        message:"Email already taken/ Incorrect Inputs"
+    })
+   } 
+
+   const ExistingUser =  User.findOne({
+    username: body.username
+   })
+
+   if(ExistingUser._id) {
+    return res.json({
+        message: "Email already taken/ Incorrect inputs"
+    })
+   }
+
+
+   const dbUser = await User.create(body);
+   const token = jwt.sign({
+    userId:dbUser._id
+   },JWT_SECRET)
+   res.json({
+    message:"user created successfully",
+    token:token
+   })
+})
 
 module.exports = router;
