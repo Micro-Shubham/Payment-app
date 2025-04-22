f7e2130b636ed1e38aa2898f8bfe8c551c14ad8f
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-22 07:02:44 +0530

    created routes for user

--- Backend/index.js
+++ Backend/index.js
@@ -3,4 +3,4 @@ const mainRouter = require("./routes/index")
 
 const app = express();
 
-app.use("/api/v1/",mainRouter)
+app.use("/api/v1/", mainRouter)
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,3 +1,12 @@
 const express = require("express")
 const router = express.Router();
+const userRouter = require("./user")
+
+
+router.use("/user",userRouter)
+
+
+
+
+
 module.exports = router;
