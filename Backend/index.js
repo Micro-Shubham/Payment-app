49ccc1b3a26b8b20f3d47fc63ffd2aeee4e480f5
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-22 17:50:21 +0530

    add JWT_SECRET configuration and update package dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,18 @@ const mainRouter = require("./routes/index")
 const app = express();
 // apply cors to all routes
 app.use(cors());
+// json body parsor
+app.use(express.json())
 
 
 
 app.use("/api/v1/",  mainRouter)
+
+
+
+
+const PORT = 3000;
+app.listen(PORT,() => {
+    console.log(`server running at ${PORT}`);
+
+}) 
