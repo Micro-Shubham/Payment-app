f5dac1f4c0ed9188415f6e35623f09a7e70ee50e
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-22 07:14:52 +0530

    added cors to all routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const cors = require('cors')
 const mainRouter = require("./routes/index")
-
 const app = express();
+// apply cors to all routes
+app.use(cors());
 
-app.use("/api/v1/", mainRouter)
+
+
+app.use("/api/v1/",  mainRouter)
