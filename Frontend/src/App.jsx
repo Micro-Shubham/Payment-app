08dbd1424db2ebc054b7a7a1d5d575d374a9fe94
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-05-01 12:16:48 +0530

    feat: add Users component with user search functionality and display

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
+import {Dashboard} from "./pages/Dashboard"
+import {SendMoney} from "./pages/SendMoney"
 
 function App() {
 
@@ -11,8 +13,8 @@ function App() {
        <Routes>
         <Route path="/signup" element={<Signup />} /> 
         <Route path="/signin" element={<Signin />} /> 
-        {/* <Route path="/dashboard" element={<Dashboard />} />  */}
-        {/* <Route path="/send" element={<SendMoney />} />  */}
+        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/send" element={<SendMoney />} /> 
        </Routes>
        </BrowserRouter>
 
