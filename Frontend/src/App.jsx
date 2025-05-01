b3c3d8e11056141c5c16c5047a48f481a0b58161
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-05-01 07:10:18 +0530

    feat: add react-router-dom for routing and update App component structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,22 @@
-
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
 
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+       <BrowserRouter>
+       <Routes>
+        <Route path="/signup" element={<signup />} /> 
+        <Route path="/signin" element={<signin />} /> 
+        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/send" element={<SendMoney />} /> 
+       </Routes>
+       </BrowserRouter>
+
+
+    </>
   )
+
 }
 
 export default App
