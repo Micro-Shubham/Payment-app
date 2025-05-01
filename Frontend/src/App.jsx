59033ddf7edffcb27b8fd1f0295fabdceca7bd34
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-05-01 11:11:15 +0530

    feat: fix component imports in App and add Appbar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+
 function App() {
 
   return (
     <>
        <BrowserRouter>
        <Routes>
-        <Route path="/signup" element={<signup />} /> 
-        <Route path="/signin" element={<signin />} /> 
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/signin" element={<Signin />} /> 
         <Route path="/dashboard" element={<Dashboard />} /> 
         <Route path="/send" element={<SendMoney />} /> 
        </Routes>
