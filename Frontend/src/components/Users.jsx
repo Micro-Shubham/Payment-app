cb80dfbaaa1cad92865f87ec9cf7cacdf4d9669f
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-05-02 10:28:12 +0530

    feat: enhance Users component with API integration and search functionality

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -1,36 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./Button";
+import axios from "axios"
+import {useNavigate} from "react-router-dom"
 
 export const Users = () => {
   // replace with backend call
-  const [users, setUsers] = useState([
-    {
-      firstName: "Harkirat",
-      lastName: "Singh",
-      _id: 1,
-    },
-  ]);
+  const [users, setUsers] = useState([])
+  const [filter, setFilter] = useState("")
+
+
+  useEffect(() => {
+    axios.get("https://localhost:3000/api/v1/bulk?filter="+filter)
+    .then(response =>{
+      setUsers(response.data.user)
+    })
+
+  },[filter])
 
   return (
     <>
       <div className="font-bold mt-6 text-lg">Users</div>
       <div className="my-2">
-        <input
+        <input onChange={(e) =>{
+          setFilter(e.target.value)
+        }}
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded borderp-slate-200"
         ></input>
       </div>
       <div>
-        {users.map((user) => (
+        {users.map(user => 
           <Users user={user} />
-        ))}
+        )}
       </div>
     </>
   );
 };
 
 function User({ user }) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between">
       <div className="flex">
@@ -47,7 +57,9 @@ function User({ user }) {
       </div>
 
       <div className="flex flex-col justify-center h-full">
-        <Button label={"send money"} />
+        <Button onclick={(e) =>{
+          navigate("/send?id="+user._id + "&name=" + user.firstName)
+        }} label={"send money"} />
       </div>
     </div>
   );
